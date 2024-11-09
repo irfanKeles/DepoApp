cafb9120c9ace29fdd842a8f6e54cf3ab39bc645
İrfan Keleş <irfn.ak47@gmail.com>
2024-11-09 17:03:14 +0300

    header currency tasarımı yapıldı

--- src/app/@layout/header/header.component.ts
+++ src/app/@layout/header/header.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Currency } from '../../@model/currency';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  currenncy:Currency[] = [];
+  constructor(
+    private _http: HttpClient,
+  ) {
+
+  }
+
+  ngOnInit() {
+    // this._http.get('https://v6.exchangerate-api.com/v6/03000b0ffef4008088e540a9/latest/USD').subscribe((x:any) => {
+    //   this.currenncy = [
+    //     { code: 'USD', rate: x.conversion_rates.USD },
+    //     { code: 'EUR', rate: x.conversion_rates.EUR },
+    //     { code: 'GBP', rate: x.conversion_rates.GBP }
+    //   ];
+    //   console.log(this.currenncy)
+    // },
+    //   err => {
+    //     console.log(err);
+    //   })
+  }
 
 }
---
+++ src/app/@model/currency.ts
@@ -0,0 +1,4 @@
+export class Currency{
+    code?:string;
+    rate?:number;
+}
