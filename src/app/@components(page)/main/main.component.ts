7a1286452a1f4b14158a0336187e093072185c42
İrfan Keleş <irfn.ak47@gmail.com>
2024-11-09 17:28:24 +0300

    primeng kütğüphanesi indirildi

--- src/app/@components(page)/main/main.component.ts
+++ src/app/@components(page)/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [DashboardComponent],
+  imports: [DashboardComponent,ButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
